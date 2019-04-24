bc1feed9c714ea909935c654ac4270718501da1c
alcomen <alcomen@gmail.com>
2019-04-24 19:14:24 -0300

    Add files via upload
    Versão já com sensores IR implementados e controle de velocidade via PWM

--- Robo_Segue_Faixa.ino
+++ Robo_Segue_Faixa.ino
@@ -1,4 +1,9 @@
 
+/*************************************************************/
+/*  Colégio Univap CTI - 2019
+ *   Código para testes do Robô segue linha
+ */
+
 // Pinos de controle do motor na Ponte H
 #define IN1       13      
 #define IN2       12
@@ -6,16 +11,19 @@
 #define IN4       10
 
 // Pinos de controle da velocidade do motor na Ponte H
-#define ENA_PIN   5
-#define ENB_PIN   6
+#define ENA_PIN   6
+#define ENB_PIN   9
 
 // Velocidade de cada motor
-#define M1_VEL    50  // Motor direita
-#define M2_VEL    50  // Motor esquerda
+#define M1_VEL    30  // Motor direita
+#define M2_VEL    30  // Motor esquerda
 
 // Pinos dos sensores segue faixa
-#define SENSOR_D  9   // Sensor da direita
-#define SENSOR_E  8   // Sensor da esquerda
+#define SENSOR_D  4   // Sensor da direita
+#define SENSOR_E  5   // Sensor da esquerda
+
+int vel_motor_d, vel_motor_e;
+
 
 void frente(void)
 {
@@ -82,28 +90,31 @@ void setup() {
   analogWrite(ENA_PIN, 127);
   analogWrite(ENB_PIN, 127);
 
+  analogWrite(ENA_PIN, map(M1_VEL, 0, 100, 0, 255));
+  analogWrite(ENB_PIN, map(M1_VEL, 0, 100, 0, 255));
+
+  //frente();
+  //direita();
+  //while(1);
   parar();
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int vel_motor_d, vel_motor_e;
+  //if(digitalRead(SENSOR_D) && digitalRead(SENSOR_E)) parar();else
 
-  if(digitalRead(SENSOR_D) && digitalRead(SENSOR_E)) parar();else
-
-    if(SENSOR_D)
+    if(digitalRead(SENSOR_E))
     {
       direita();
+      //Serial.println("Sensor D");
     }else
-      if(SENSOR_E)
+      if(digitalRead(SENSOR_D))
       {
         esquerda();
-      }    
+        //Serial.println("Sensor E");
+      }else
+       //Serial.println("");
   frente();
 
-  analogWrite(ENA_PIN, map(M1_VEL, 0, 100, 0, 255));
-  analogWrite(ENB_PIN, map(M1_VEL, 0, 100, 0, 255));
-
-  delay(100);
 }
